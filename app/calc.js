0e8e108ee33df4a57aa0e9554481792bb8f25441
mort.vicious <shindo.shitai@gmail.com>
2020-05-11 18:05:14 +0400

    Refactoring

---
+++ app/calc.js
@@ -0,0 +1,184 @@
+
+     (function() {
+
+    const operators = document.querySelectorAll(`.operators`)
+    const integers = document.querySelectorAll(`.btn-int`)
+    // const secondary = document.querySelectorAll(`.secondary-btn`)
+    const display = document.querySelector(`.display`)
+    const clearButton = document.querySelector(`.btn-clear`)
+    const btnDot = document.querySelector(`.btn-dot`)
+    const btnBack = document.querySelector(`.btn-back`)
+  
+    const oper = [`+`, `-`, `*`, `/`]
+    let op
+  
+    let hasDot = false
+    let typed = false
+    let hasInt = false
+    let a
+    let b 
+    let c 
+  
+    (function() {
+         clearButton.addEventListener(`click`, reset)
+         btnDot.addEventListener(`click`, checkDot)
+         btnBack.addEventListener(`click`, backspace)
+    })();
+  
+    (function() {
+         integers.forEach(integer => {
+              integer.addEventListener(`click`, (number) => {
+                   number = integer.textContent
+                   hasInt = true
+                   setDisplay(number)
+                   checkInt()
+              })
+         })
+    })();
+  
+    (function() {
+         operators.forEach(operator => {
+              operator.addEventListener(`click`, () => {
+                   clean()
+                   checkOperator(operator)
+                   checkEqual(operator)
+                   hasDot = false
+              })
+         })
+    })();
+  
+    function reset() {
+         a = undefined
+         b = undefined
+         typed = false
+         hasInt = false
+         clean()
+         op = undefined
+         console.log(`Reset. A: ${a}, B: ${b}, Operator: ${op}`)
+    }
+  
+    function enter() {
+         return parseFloat(display.textContent)
+    }
+  
+    function sum() {
+         c = a + b
+         return c
+    }
+  
+    function sub() {
+         c = a - b
+         return c
+    }
+  
+    function mul() {
+         c = a * b
+         return c
+    }
+  
+    function div() {
+         c = a / b
+         return  c
+    }
+  
+    function clean() {
+         display.textContent = ``
+    }
+  
+    function setDisplay(s) {
+         display.textContent += `${s}`
+    }
+  
+    function setOp(s) {
+         op = s
+    }
+  
+    function backspace(str, newStr) {
+         console.log(`clicked`)
+         str = display.textContent
+         newStr = str.slice(0, -1)
+         display.textContent = newStr
+              if (typed === false) {
+                   a = parseFloat(display.textContent)
+              } else if (typed === true) {
+                   b = parseFloat(display.textContent)
+              }
+    }
+  
+    function checkInt() {
+         if (typed === false) {
+              a = enter()
+              console.log(`Declaration of A: ${a}`)////////
+              console.log(`hasInt: ${hasInt}`)
+         }
+         if (typed === true) {
+              b = enter()
+              console.log(`Declaration of B: ${b}`)/////////
+         }
+    }
+  
+    function checkDot(s) {
+         if(hasDot === false && hasInt === true) {
+              console.log(hasDot)
+              hasDot = true
+              s = `.`
+              setDisplay(s)
+              if (hasDot === true) {
+                   console.log(`aw`)
+                   return
+              }
+         }
+    }
+
+    function checkOperator(operator) {
+        if(hasInt === false && operator.classList.contains(`btn-sub`)) {
+            display.textContent += `-`
+        }
+         if (typed === false && hasInt === true) {
+              if(operator.classList.contains(`btn-add`)) {
+                   setOp(oper[0])
+              }
+              if(operator.classList.contains(`btn-mul`)) {
+                   setOp(oper[2])
+              }
+              if(operator.classList.contains(`btn-div`)) {
+                   setOp(oper[3])
+              }
+              if(operator.classList.contains(`btn-sub`)) {
+                   setOp(oper[1])
+              }         
+              typed = true  
+         }
+         console.log(`Operator: ${op}, Cleaning display, cleaning logic input`)/////////
+    }
+  
+    function checkEqual(operator) {
+         if (typed === true && hasInt === true) {
+              if(operator.classList.contains(`btn-equal`)) {
+                   if (op == oper[0]) {
+                        sum()
+                   }
+                   if (op == oper[2]) {
+                        mul()
+                   }
+                   if (op == oper[3]) {
+                        if (b == 0) {
+                             reset()
+                             return
+                        } else {
+                             div()
+                        }
+                   }
+                   if (op == oper[1]) {
+                        sub()
+                   }
+                   setDisplay(c)
+                   typed = false
+                   a = c
+                   console.log(`Redeclaration of A: ${c}, Result: ${c}`)
+              }
+         } 
+    }
+  })();
+  
+  
